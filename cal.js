e9dec24f73b3c18447a1aa816984010f76a25f6b
Manan _Jain <77162837+manan2504@users.noreply.github.com>
2021-07-17 17:16:55 +0530

    cal

---
+++ cal.js
@@ -0,0 +1,17 @@
+function getHistory(){
+	return document.getElementById("history-value").innerText;
+}
+function printHistory(num){
+	document.getElementById("history-value").innerText=num;
+}
+function getOutput(){
+	return document.getElementById("output-value").innerText;
+}
+function printOutput(num){
+	if(num==""){
+		document.getElementById("output-value").innerText=num;
+	}
+	else{
+		document.getElementById("output-value").innerText=getFormattedNumber(num);
+	}	
+}
